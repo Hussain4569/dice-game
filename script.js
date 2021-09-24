3139ef1952b0c9cc838da47795963204fec6427d
Hussain4569 <88427896+Hussain4569@users.noreply.github.com>
2021-09-24 18:11:02 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,60 +1,15 @@
-var randomNum1 = Math.floor((Math.random() * 6) + 1);
-var randomNum2 = Math.floor((Math.random() * 6) + 1);
-// var playerOne = prompt("Enter name of Player 1.");
-// var playerTwo = prompt("Enter name of Player 2.");
-var flagOne = document.querySelector("i.dice-1-flag");
-var flagTwo = document.querySelector("i.dice-2-flag");
+var randomNumber1 = Math.floor(Math.random() * 6) +1;
+var firstImgName = "dice" + randomNumber1 + ".png";
+document.querySelectorAll('img')[0].setAttribute("src" , "images/" + firstImgName);
 
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var secondImgName = "dice" + randomNumber2 + ".png";
+document.querySelectorAll('img')[1].setAttribute("src" , "images/" + secondImgName);
 
-// function changeNames(){
-//   document.querySelector("h2.player-one").innerHTML = playerOne;
-//   document.querySelector("h2.player-two").innerHTML = playerTwo;
-// }
-
-function changeHeading(){
-  if (randomNum1 > randomNum2) {
-    document.querySelector("h1").innerHTML = '<i class="fas fa-flag"></i> Player 1 has won.';
-  } else if (randomNum1 < randomNum2) {
-    document.querySelector("h1").innerHTML = 'Player 2 has won. <i class="fas fa-flag"></i>';
-  } else if (randomNum1 === randomNum2) {
-    document.querySelector("h1").innerHTML = 'Draw.';
-  } else {
-    document.querySelector("h1").innerHTML = 'Refresh me.';
-   }
+if (randomNumber1 > randomNumber2) {
+  document.querySelector('h1').innerHTML = "🚩 Player 1 wins";
+} else if (randomNumber2 > randomNumber1) {
+  document.querySelector('h1').innerHTML = "Player 2 wins 🚩";
+} else {
+  document.querySelector('h1').innerHTML = "Draw";
 }
-
-function changeDiceOne(){
-  if (randomNum1 === 1) {
-    document.querySelector("img.first-dice").setAttribute("src", "images/dice1.png");
-  } else if (randomNum1 === 2) {
-    document.querySelector("img.first-dice").setAttribute("src", "images/dice2.png");
-  } else if (randomNum1 === 3) {
-    document.querySelector("img.first-dice").setAttribute("src", "images/dice3.png");
-  } else if (randomNum1 === 4) {
-    document.querySelector("img.first-dice").setAttribute("src", "images/dice4.png");
-  } else if (randomNum1 === 5) {
-    document.querySelector("img.first-dice").setAttribute("src", "images/dice5.png");
-  } else {
-    document.querySelector("img.first-dice").setAttribute("src", "images/dice6.png");
-  }
-}
- function changeDiceTwo(){
-  if (randomNum2 === 1) {
-    document.querySelector("img.second-dice").setAttribute("src", "images/dice1.png");
-  }  else if (randomNum2 === 2) {
-    document.querySelector("img.second-dice").setAttribute("src", "images/dice2.png");
-  } else if (randomNum2 === 3) {
-    document.querySelector("img.second-dice").setAttribute("src", "images/dice3.png");
-  } else if (randomNum2 === 4) {
-    document.querySelector("img.second-dice").setAttribute("src", "images/dice4.png");
-  } else if (randomNum2 === 5) {
-    document.querySelector("img.second-dice").setAttribute("src", "images/dice5.png");
-  } else {
-    document.querySelector("img.second-dice").setAttribute("src", "images/dice6.png");
-  }
- }
-
-//changeNames();
-changeHeading();
-changeDiceOne();
-changeDiceTwo();
